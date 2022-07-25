451d32541987edcba5241f39f35050cbf19c1e96
Aizen <caionascimento60@gmail.com>
2022-07-25 15:47:45 -0300

    dia 2 terminado

--- Modulos/01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Modulos/01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,1 +1,33 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     soma += numbers[index];
+
+    
+// }
+
+// let media = soma / numbers.length
+
+// if (media > 20 ) {
+//     console.log("valor maior que 20");
+// }
+// else {
+//     console.log("valor menor que 20");
+// }
+
+// console.log(media);
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if(numbers[index] % 2 !== 0){
+        soma += 1;
+    } 
+}
+
+
+if (soma === 0) {
+    console.log("não tem numeros impars");    
+}
+else {
+    console.log(soma);
+}
