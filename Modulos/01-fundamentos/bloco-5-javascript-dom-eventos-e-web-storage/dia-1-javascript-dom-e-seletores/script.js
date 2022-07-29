282bbcc8721c483197520dc117a5b7434865fb1a
Aizen <caionascimento60@gmail.com>
2022-07-29 15:34:43 -0300

    resto do exercicio

--- Modulos/01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ Modulos/01-fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,1 +1,21 @@
-document
+document.querySelectorAll('h1')[0].style.background = 'rgb(36, 204, 134)';
+
+document.querySelectorAll('h1')[0].style.border = '15px solid rgb(36, 204, 134)';
+
+document.querySelectorAll('#header-container')[0].style.padding = '0';
+
+document.querySelectorAll('.emergency-tasks')[0].style.background = 'rgb(231, 121, 114)';
+
+document.querySelector('.emergency-tasks h3').style.background = 'rgb(135, 30, 196)';
+
+document.querySelectorAll('.emergency-tasks h3')[1].style.background = 'rgb(135, 30, 196)';
+
+document.querySelectorAll('.no-emergency-tasks')[0].style.background = 'rgb(245, 191, 92)';
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.background = 'rgba(0, 0, 0, 0.959)';
+
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.background =  'rgba(0, 0, 0, 0.959)';
+
+document.querySelector('#footer-container').style.background = 'rgba(0, 0, 0, 0.671)';
+
+document.querySelector('#footer-container')[0].style.padding = '0';
